8263f4162ec698ae35adfe5703dea73ea243d4a6
Kira <56455045+kiracherie@users.noreply.github.com>
2020-11-14 16:03:48 -0800

    initial commit

---
+++ app/src/main/java/com/example/firebasetest/SampleData.java
@@ -0,0 +1,38 @@
+package com.example.firebasetest;
+
+public class SampleData {
+    private String id;
+    private String name;
+    private String description;
+
+    SampleData() {}
+
+    SampleData(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
